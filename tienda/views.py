799b8c054bff9daa38b4d65685649ef25b1444ac
Squawks1 <to.toledom@duocuc.cl>
2026-07-18 23:11:43 -0400

    Update views.py
    Se generan views de la app

--- tienda/views.py
+++ tienda/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
 
-# Create your views here.
+def inicio(request):
+    return render(request, 'index.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def registro(request):
+    return render(request, 'registro.html')
+
+
+def productos(request):
+    return render(request, 'productos.html')
+
+
+def detalle_producto(request):
+    return render(request, 'detalle_producto.html')
+
+
+def carrito(request):
+    return render(request, 'carrito.html')
+
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+
+def compra_exitosa(request):
+    return render(request, 'compra_exitosa.html')
+
+
+def pedidos(request):
+    return render(request, 'pedidos.html')
+
+
+def estado_pedido(request):
+    return render(request, 'estado_pedido.html')
+
+
+def admin_panel(request):
+    return render(request, 'admin_panel.html')
+
+
+def admin_productos(request):
+    return render(request, 'admin_productos.html')
+
+
+def admin_pedidos(request):
+    return render(request, 'admin_pedidos.html')
